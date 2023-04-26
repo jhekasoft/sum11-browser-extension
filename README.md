1b05866e7e6c7a9032b0f533582074880974a21f
Eugene Efremov <jhekasoft@gmail.com>
2023-04-26 18:36:56 +0300

    Create SUM-11 extension

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# СУМ-11. Словник української мови
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import logo from './logo.png';
+// import logo from './sum11.jpg';
 import './App.css';
 
 function App() {
+  // const [url, setUrl] = useState<string>('');
+  const [keyword, setKeyword] = useState('');
+
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    // Preventing the page from reloading
+    event.preventDefault();
+
+    // Open new tab
+    chrome.tabs && chrome.tabs.create({url: `http://sum.in.ua/?swrd=${keyword}`, selected: true, active: true});
+  }
+
+  async function getActiveTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let tabs = await chrome.tabs.query(queryOptions);
+    return tabs[0].id;
+  }
+
+  /**
+ * Get current URL
+ */
+  useEffect(() => {
+    const fetchData = async () => {
+      const tabId = await getActiveTab();
+      chrome.scripting && tabId && chrome.scripting.executeScript({
+        target: { tabId },
+        func: () => {
+          const selection = window.getSelection();
+          return selection ? selection.toString() : "";
+        }
+      }, (res) => {
+        if (res.length) {
+          setKeyword(res[0].result);
+        }
+      });
+    }
+
+    fetchData().catch(console.error);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          СУМ-11. Словник української мови
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submitForm}>
+          <input
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            type="text"
+            placeholder="Введіть чи виділіть слово"
+            className="input"
+            autoFocus
+          />
+          <button type="submit" className="btn">Шукати</button>
+        </form>
+        <p>
+          <a
+            className="App-link"
+            href="https://efremov.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            efremov.dev
+          </a>
+        </p>
       </header>
     </div>
   );
